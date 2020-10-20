09f5ccb1529db369988afb12d81ecbdc880b248c
Tortique <adrien86190@yahoo.fr>
2020-10-20 12:05:19 +0200

    modification pour le long terme

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,40 +1,24 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated")) {
-				pupilCount++;
-			}
+    public static void main(String args[]) throws Exception {
+        /**
+         * Construct a list with input file symptoms.txt
+         */
+        ISymptomReader reader = new ReadSymptomDataFromFile("Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+        /**
+         * Construct an output file for result
+         */
+        FileWriter writer = new FileWriter("result.out");
+        /**
+         * Clean and sort the list
+         * Write in output file, symptoms with their occurrence.
+         */
+        ProcessOut result = new ProcessOut(reader, writer);
+        result.ProcessOut();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    }
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ProcessOut.java
@@ -0,0 +1,42 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class ProcessOut {
+
+    /**
+     * Properties
+     */
+    private ISymptomReader reader;
+    private FileWriter writer;
+
+    /**
+     * Constructor
+     *
+     * @param reader refer to object instance of ReadSymptomDataFromFile
+     * @param writer refer to object instance of output file
+     */
+    public ProcessOut(ISymptomReader reader, FileWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    /**
+     * Transform the list to a HashSet to keep just one copy of each symptoms
+     * Transform it to list for sort alphabetically
+     *
+     * @for each element of the list : write in output file his name and his occurrences
+     */
+    public void ProcessOut() throws IOException {
+        Set<String> test = new HashSet<>(reader.GetSymptoms());
+        List<String> unique = new ArrayList<>(test);
+        Collections.sort(unique);
+        for (String elem : unique) {
+            writer.write(elem + " " + Collections.frequency(reader.GetSymptoms(), elem) + "\n");
+        }
+        writer.close();
+    }
+
+}
